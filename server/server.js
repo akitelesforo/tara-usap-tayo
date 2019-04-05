c3113cac52e1cbf2b1e1bac1b4bd5f05b6d334f6
Akito Telesforo <akitelesforo@yahoo.com>
2019-04-05 14:00:29 +0800

    Merges angularjs and nodejs

--- server/server.js
+++ server/server.js
@@ -7,7 +7,24 @@ let server = http.Server(app);
 let socketIO = require('socket.io');
 let io = socketIO(server);
 
-const port = process.env.OPENSHIFT_INTERNAL_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
+// App
+app.use(function(req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', false);
+
+    next();
+});
+
+// Routing
+app.use(express.static(__dirname + '/public'));
+
+
+// Socket.io
+const ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+const port = process.env.OPENSHIFT_NODEJS_PORT || 8000;
 
 io.set('log level', 1);
 
@@ -37,6 +54,6 @@ io.on('connection', (socket) => {
 
 });
 
-server.listen(port, () => {
+server.listen(port, ipaddress, () => {
     console.log(`Socket IO server started on port: ${port}`);
 });
