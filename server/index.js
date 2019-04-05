462bdb06dfbc0fc7b5159bb2a0f55b92164ab860
Akito Telesforo <akitelesforo@yahoo.com>
2019-04-05 11:37:20 +0800

    Api changes

--- server/index.js
+++ server/index.js
@@ -7,7 +7,16 @@ let server = http.Server(app);
 let socketIO = require('socket.io');
 let io = socketIO(server);
 
-const port = process.env.PORT || 3000;
+const port = process.env.OPENSHIFT_INTERNAL_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
+
+io.enable('browser client minification');
+io.enable('browser client etag');
+io.enable('browser client gzip');
+io.set('log level', 1);
+
+io.set('transports', [
+    'websocket'
+]);
 
 io.on('connection', (socket) => {
 
